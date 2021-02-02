381e4ccb18d8d5052237774acd02b191c5a1ed4c
melissa-stott <1princessmelissa@gmail.com>
2021-02-01 23:28:51 -0800

    wrote get commands to access homepage and notes page

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
 const express = require('express');
 const PORT = 8080;
+const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('../../../public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
+
+// app.get('/', (req, res) => res.sendFile('index.html', {root: __dirname + '/..'}));
+
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
