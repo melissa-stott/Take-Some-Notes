2cf4af33830a674bce87d28200075f16a711c049
melissa-stott <1princessmelissa@gmail.com>
2021-02-03 07:55:34 -0800

    fixed pathways to connect to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 let db = '';
 const { v4: uuidv4 } = require('uuid');
-fs.readFile('../../../db/db.json','utf8', (error, data) =>
+fs.readFile('./db/db.json','utf8', (error, data) =>
 {
     if (error) {
         throw error;
@@ -17,9 +17,9 @@ fs.readFile('../../../db/db.json','utf8', (error, data) =>
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('../../../public'));
+app.use(express.static('./public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.get('/api/notes', (req, res) => {
     res.send(JSON.stringify(db))
@@ -34,11 +34,11 @@ app.post('/api/notes', (req, res) => {
         text: newNote.text
     };
     db.push(newText);
-    fs.writeFile('../../../db/db.json', JSON.stringify(db), (err) =>
+    fs.writeFile('../db/db.json', JSON.stringify(db), (err) =>
     err ? console.error(err) : console.log('Success!'));
     return res.status(200).send();
 })
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
